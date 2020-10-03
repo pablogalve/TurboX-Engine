9e99dba2fb0471a539a2ce474d6f9cd0c5bc6c14
maciadalmau <47743853+maciadalmau@users.noreply.github.com>
2020-10-03 19:32:47 +0200

    Intregating MathGeoLib and removing SDL_Mixer

--- TurboX-Engine/TurboX-Engine/ModuleAudio.h
+++ TurboX-Engine/TurboX-Engine/ModuleAudio.h
@@ -2,7 +2,6 @@
 #define __ModuleAudio_H__
 
 #include "Module.h"
-#include "SDL_mixer\include\SDL_mixer.h"
 
 #define DEFAULT_MUSIC_FADE_TIME 2.0f
 
@@ -27,8 +26,7 @@ public:
 
 private:
 
-	Mix_Music*			music;
-	p2List<Mix_Chunk*>	fx;
+	
 };
 
 #endif // __ModuleAudio_H__
