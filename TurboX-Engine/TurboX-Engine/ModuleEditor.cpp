2797f73f2136db42415215dd6afc51a34606da7c
maciadalmau <47743853+maciadalmau@users.noreply.github.com>
2020-10-15 18:18:25 +0200

    merge

--- TurboX-Engine/TurboX-Engine/ModuleEditor.cpp
+++ TurboX-Engine/TurboX-Engine/ModuleEditor.cpp
@@ -115,7 +115,7 @@ void ModuleEditor::ShowMenuBar()
 		}
 		if (ImGui::BeginMenu("Assets")) {
 			if (ImGui::MenuItem("Create Cube"))App->renderer3D->DrawCubeWithIndices();
-			if (ImGui::MenuItem("Create Sphere"))App->renderer3D->DrawSphere();
+			if (ImGui::MenuItem("Create Sphere"))App->renderer3D->DrawSphere(1, 12, 24);
 			if (ImGui::MenuItem("Create Pyramid"))App->renderer3D->DrawPyramid();
 			if (ImGui::MenuItem("Create Cylinder"))App->renderer3D->DrawCylinder();
 
