196313bd32ccc0b2f3b35eee1d3bade6cb2005cc
Pablo <eduinvestor100@gmail.com>
2021-01-04 14:41:04 +0100

    opengl fixed

--- TurboX-Engine/Source_Code/Libraries/ImGui/imgui_impl_opengl3.h
+++ TurboX-Engine/Source_Code/Libraries/ImGui/imgui_impl_opengl3.h
@@ -66,7 +66,7 @@ IMGUI_IMPL_API void     ImGui_ImplOpenGL3_DestroyDeviceObjects();
 
 // Otherwise try to detect supported Desktop OpenGL loaders..
 #elif defined(__has_include)
-#if __has_include(<glew/glew.h>)
+#if __has_include("../glew/glew.h")
     #define IMGUI_IMPL_OPENGL_LOADER_GLEW
 #elif __has_include(<glad/glad.h>)
     #define IMGUI_IMPL_OPENGL_LOADER_GLAD
